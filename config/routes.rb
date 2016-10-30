72bb3abf339f1c0cbdd7f6a261c54999592447ef
Alexander Riese <alexander.riese@student.hpi.de>
2016-10-30 17:50:00 +0100

    add authors resource route #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'authors/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,6 +15,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :authors
 
   # Example resource route with options:
   #   resources :products do
