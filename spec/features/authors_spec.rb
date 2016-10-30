c6645748f9eaf4664fd85b746b8215f4198acbd1
Alexander Riese <alexander.riese@student.hpi.de>
2016-10-30 20:01:42 +0100

    failing test #4

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -6,4 +6,10 @@ describe "Authors new page", :type => :feature do
     visit "/authors/new"
     expect(page).to have_field("First name")
   end
+
+  it "should display text input for last name" do
+    visit "/authors/new"
+    expect(page).to have_field("Last name")
+  end
+
 end
