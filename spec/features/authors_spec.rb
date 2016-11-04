ebb400518cb4fbf07814184737c6c13875d8992a
Alexander Riese <alexander.riese@student.hpi.de>
2016-11-04 13:16:14 +0100

    failing test #14

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -3,33 +3,43 @@ require 'rails_helper'
 describe "Authors new page", :type => :feature do
   
   it "should display text input for first name" do
-    visit "/authors/new"
-    expect(page).to have_field("First name")
+    visit '/authors/new'
+    expect(page).to have_field('First name')
   end
 
   it "should display text input for last name" do
-    visit "/authors/new"
-    expect(page).to have_field("Last name")
+    visit '/authors/new'
+    expect(page).to have_field('Last name')
   end
 
   it "should display text input for homepage" do
-    visit "/authors/new"
-    expect(page).to have_field("Homepage")
+    visit '/authors/new'
+    expect(page).to have_field('Homepage')
   end
 
   it "should display button to submit form labeled with 'Create Author'" do
-    visit "/authors/new"
-    expect(page).to have_button("Create Author")
+    visit '/authors/new'
+    expect(page).to have_button('Create Author')
   end
 
   it "should exist a database entry for a author which was submitted via the new author form" do
-	@author = {first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing"}
-    visit "/authors/new"
+	@author = {first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing'}
+    visit '/authors/new'
     fill_in 'First name', with: @author[:first_name]
     fill_in 'Last name', with: @author[:last_name]
     fill_in 'Homepage', with: @author[:homepage]
 	click_button 'Create Author'
     expect(Author.where(first_name: @author[:first_name], last_name: @author[:last_name])).to exist
+  end
+
+end
+
+describe "Authors show page", :type => :feature do
+
+  it "should show the first name of an author" do
+	author = create(:author)
+	visit "/authors/#{author.id}"
+	expect(page).to have_text("first name: #{author.first_name}")
   end
 
 end
