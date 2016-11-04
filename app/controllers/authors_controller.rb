1481b793d34e41049ea3c6360aa9ff573f064283
Alexander Riese <alexander.riese@student.hpi.de>
2016-11-04 12:42:57 +0100

    author page can be rendered fix #13

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,16 +1,18 @@
 class AuthorsController < ApplicationController
-  def new
+
+  def show
   end
 
-  def create
-    redirect_to new_author_path
+  def new
   end
 
   def create
     @author = Author.new(author_params)
 	@author.save
-	redirect_to new_author_path
+	redirect_to @author
   end
+
+  private
 
   def author_params
 	params.require(:author).permit(:first_name, :last_name, :homepage)
