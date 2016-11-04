f06d27d1ce807b3f4abf15f66b8e44aa93f9b3d4
Alexander Riese <alexander.riese@student.hpi.de>
2016-11-04 13:57:16 +0100

    failing test #18

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -36,10 +36,11 @@ end
 
 describe "Authors show page", :type => :feature do
 
-  it "should show the first name of an author" do
+  it "should show details of an author" do
     author = FactoryGirl.create(:author)
     visit "/authors/#{author.id}"
     expect(page).to have_text(author.first_name)
+    expect(page).to have_text(author.last_name)
   end
 
 end
