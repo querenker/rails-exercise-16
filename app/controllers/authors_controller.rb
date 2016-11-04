1dd38ccc0e4319ebd98eaed8fc018ca27c4ad810
Alexander Riese <alexander.riese@student.hpi.de>
2016-11-04 12:10:27 +0100

    new author page save the author in a database fix #12

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,4 +6,14 @@ class AuthorsController < ApplicationController
     redirect_to new_author_path
   end
 
+  def create
+    @author = Author.new(author_params)
+	@author.save
+	redirect_to new_author_path
+  end
+
+  def author_params
+	params.require(:author).permit(:first_name, :last_name, :homepage)
+  end
+
 end
