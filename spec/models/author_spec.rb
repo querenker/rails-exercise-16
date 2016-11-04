0642caa49df05ef1fd0fbd57a83eff43e14cb38c
Alexander Riese <alexander.riese@student.hpi.de>
2016-11-04 11:26:58 +0100

    failing test #11

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+
   it "should create an instance of author" do
     @author = build(:author)
     expect(@author).to_not be_nil
   end
+
+  it "should return the full name of an author" do
+    @author = build(:author)
+	expect(@author.name).to eq("Alan Turing")
+  end
+
 end
