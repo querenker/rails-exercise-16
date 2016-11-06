d85c3c56504a6ae50df80e8468f0aa05fd5d215a
Alexander Riese <alexander.riese@student.hpi.de>
2016-11-06 13:39:41 +0100

    failing test #45

--- spec/features/papers_spec.rb
+++ spec/features/papers_spec.rb
@@ -11,3 +11,16 @@ describe "Papers show page", :type => :feature do
   end
 
 end
+
+
+describe "Papers index page", :type => :feature do
+
+  it "should show details of an paper" do
+    paper = FactoryGirl.create(:paper)
+    visit '/papers'
+	expect(page).to have_text(paper.title)
+	expect(page).to have_text(paper.venue)
+	expect(page).to have_text(paper.year)
+  end
+
+end
