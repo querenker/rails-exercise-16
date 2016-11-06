07ca69bfdc84543aa5d380b3669e931bf7657869
Alexander Riese <alexander.riese@student.hpi.de>
2016-11-06 14:44:36 +0100

    added destroy link for paper to paper index page fix #48

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -34,6 +34,12 @@ class PapersController < ApplicationController
     end
   end
 
+  def destroy
+    @paper = Paper.find(params[:id])
+	@paper.destroy
+	redirect_to papers_path
+  end
+
   private
 
   def paper_params
